c4cf0841aae22587f5e24a0f7bf75b3dd1992390
ajp <ajp@TEMIS.alamo.local>
2015-11-07 13:27:53 +0100

    Identificadores y segunda versión Hola2.

---
+++ Demos2015/src/practica1/Identif.java
@@ -0,0 +1,92 @@
+package practica1;
+/**
+ * Identif.java
+ * Programa que comprueba la validez de identifcadores.
+ * ajp - 2015.10.7
+ */
+
+public class Identif {
+
+	public static void main(String[] args) {	
+
+		int _alpha;
+
+		int FLOAT;				// Correcto pero no recomendable
+
+//		int 1_de_muchos;		// No se admite empieza por número
+
+		int maxValor;
+
+		int cuantos;
+
+//		int	"dado";				// No se admiten " caracter especial 
+	
+		int Nbytes;         
+
+//		int pink.panter;		// No se admite . caracter especial
+
+//		int qué_dices?			// No se admiten ? caracter especial
+
+		int Número;
+
+//		int cadena 2;			// No se admiten espacio
+
+		int Cañón;
+
+		int café;				
+
+//		int Mesa-3;				// No se admiten - caracter especial
+
+		int Return;				// Correcto pero no recomendable
+
+		int While;				// Correcto pero no recomendable
+
+//		int int;				// No se admiten palabras reservadas
+
+		int __if;				// Correcto pero no recomendable
+
+//		int Bloque#4;			// No se admiten # caracter especial
+
+//		int c o s a; 			// No se admiten espacios
+
+		int CaPrIcHoSo;
+
+//		int 8ªRoca;				// No se admite empieza por número
+
+//		int 3d2;				// No se admite empieza por número
+
+//		int Hoja3/5;			// No se admiten / caracter especial 
+
+// Nuevos		
+
+
+		int Integer;			// Correcto pero no recomendable
+
+		int $1DeMuchos;			// Correcto pero no recomendable
+
+		int máximoº;
+
+//		int ¿Cuántos?;			// No se admiten ¿? caracteres especiales
+
+		int	precioEn€;			 
+
+		int nombre;         
+		
+//		int pink.panter;		// No se admite . caracter especial
+
+//		int qué_dices?			// No se admiten ? caracter especial
+
+		int Nûmero;
+
+		int Nùmero;
+
+		int garçon;			
+			
+		//int for;				// No se admiten palabras reservadas
+
+		int For;				// Correcto pero no recomendable
+
+		int _8ªRoca;			
+
+	}
+}
