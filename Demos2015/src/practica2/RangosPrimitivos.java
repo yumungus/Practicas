4a68a5d612ba73a9505d6d18e500a60aa5b0d253
ajp <ajp@TEMIS.alamo.local>
2015-11-07 13:37:08 +0100

    práctica2, rangos de tipos primitivos.

---
+++ Demos2015/src/practica2/RangosPrimitivos.java
@@ -0,0 +1,40 @@
+package practica2;
+
+public class RangosPrimitivos {
+
+	public static void main(String[] args) {
+
+		System.out.println("Valor máximo tipo char: " + Character.MAX_VALUE);
+		System.out.println("Valor mínimo tipo char: " + Character.MIN_VALUE + '\n');
+
+		System.out.println("Valor máximo tipo byte: " + Byte.MAX_VALUE);
+		System.out.println("Valor mínimo tipo byte: " + Byte.MIN_VALUE + '\n');
+
+		System.out.println("Valor máximo tipo short: " + Short.MAX_VALUE);
+		System.out.println("Valor mínimo tipo short: " + Short.MIN_VALUE + '\n');
+
+		System.out.println("Valor máximo tipo int: " + Integer.MAX_VALUE);
+		System.out.println("Valor mínimo tipo int: " + Integer.MIN_VALUE + '\n');
+
+
+		System.out.println("Valor máximo tipo int: " + Long.MAX_VALUE);
+		System.out.println("Valor mínimo tipo int: " + Long.MIN_VALUE + '\n');
+
+
+		System.out.println("Valor máximo tipo float: " + Float.MAX_VALUE);
+		System.out.println("Valor mínimo tipo float: " + Float.MIN_VALUE);
+		System.out.println("Valor máximo exponente float: " + Float.MAX_EXPONENT);
+		System.out.println("Valor mínimo exponente float: " + Float.MIN_EXPONENT);
+		System.out.println("Valor infinito positivo float: " + Float.POSITIVE_INFINITY);
+		System.out.println("Valor infinto negativo float: " + Float.NEGATIVE_INFINITY + '\n');
+
+		System.out.println("Valor máximo tipo double: " + Double.MAX_VALUE);
+		System.out.println("Valor mínimo tipo double: " + Double.MIN_VALUE);
+		System.out.println("Valor máximo exponente double: " + Double.MAX_EXPONENT);
+		System.out.println("Valor mínimo exponente double: " + Double.MIN_EXPONENT);
+		System.out.println("Valor infinito positivo double: " + Double.POSITIVE_INFINITY);
+		System.out.println("Valor infinto negativo double: " + Double.NEGATIVE_INFINITY + '\n');
+
+	}
+
+}
